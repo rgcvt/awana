3df5c71b2f790eb22e362789045682a87e925354
Andrew Hale <services.hale.adh@gmail.com>
2023-09-21 03:56:08 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +8,6 @@ export default defineConfig({
 		preprocessorOptions: {
 			scss: {},
 		},
-	},
-	define: {
-		'process.env': {},
 	},
 	plugins: [
 		vue({
