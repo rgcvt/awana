6954fa54dbbbb7911b991d8459ffd936ed6203af
Andrew Hale <services.hale.adh@gmail.com>
2024-09-27 00:04:44 -0400

    update sass config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler', // or "modern"
+			},
+		},
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
